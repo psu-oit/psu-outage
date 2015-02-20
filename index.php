7da7c6be554036ae18c08e93396dd0027ad15319
Elliot Schlegelmilch <elliot@pdx.edu>
2015-02-20 14:35:47 -0800

    remove seperator between host and uri; oreder of operations ?:

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
 
 
 $h1 = "Site under maintenance";
-$uri = $_SERVER["HTTPS"] ? "https://" : "http://" .$_SERVER["HTTP_HOST"] . "/" . $_SERVER["REQUEST_URI"];
+$uri = ($_SERVER["HTTPS"] ? "https://" : "http://") .$_SERVER["HTTP_HOST"] .  $_SERVER["REQUEST_URI"];
 $msg = "The web site you are trying to reach is currently down for maintenance. Please try again later.";
 
 $filename = "/var/www/html/messages/" . escapeshellcmd( $_SERVER["REQUEST_URI"]);
