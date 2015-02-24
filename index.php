77c51ba4f37ecd7aff35e26efe34afa4f050a4fd
Elliot Schlegelmilch <elliot@pdx.edu>
2015-02-24 08:39:32 -0800

    remove excessively verbose logging.

--- index.php
+++ index.php
@@ -33,8 +33,6 @@ if (file_exists($msgpath)) {
       $msg .=  $lines[$i] ."\n" ;
     }
   }
-} else {
-  error_log("No message found for service ${host}.");
 }
 ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
