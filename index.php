30e4be0f77924df1858c7c288e57a5d9185e0834
Elliot Schlegelmilch <elliot@pdx.edu>
2015-02-24 08:02:15 -0800

    seriously restructure the outage index so it can be used with multiple applications.

--- index.php
+++ index.php
@@ -1,4 +1,41 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+<?php
+header("Cache-Control: max-age=0, no-cache, no-store");
+header("Pragma: no-cache");
+header("x-outage: true");
+
+$h1 = "Site under maintenance";
+$msg = "The web site you are trying to reach is currently down for maintenance. Please try again later.";
+$pages = array(
+  '/dev\.banner\.pdx\.edu/'      => 'devel-banner',
+  '/stage.*oam\.pdx\.edu/'       => 'stage-oam',
+  '/^(access\.)?oam\.pdx\.edu/'  => 'prod-oam',
+  '/banweb\.pdx\.edu/'           => 'prod-ssb',
+  '/inb\.banner|wls\.banner/'    => 'prod-inb',
+  '/outage\.pdx\.edu/'           => 'outage'
+);
+
+foreach ($pages as $expr => $filename) {
+  $ret = preg_match($expr, $_SERVER["HTTP_HOST"]);
+  if ($ret === 1) {
+    $msgpath = "/var/www/html/messages/" . $filename;
+    break;
+  }
+}
+
+if (file_exists($filename)) {
+  $contents = file_get_contents($filename);
+  if ($contents != FALSE ) {
+    $lines = explode("\n", $contents);
+    $h1 = $lines[0];
+    $msg = '';
+    for($i=1;$i<count($lines); $i++) {
+      $msg .=  $lines[$i] ."\n" ;
+    }
+  }
+} else {
+  error_log("No message found for service " . $_SERVER["HTTP_HOST"] . ".");
+}
+?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" dir="ltr">
 
@@ -22,7 +59,7 @@
             <h1 id="site-name">
               <a href="#" title="Home" rel="home"><span>Office of Information Technology</span></a>
             </h1>
-            
+
           </div> <!-- /name-and-slogan -->
         </div> <!-- /logo-title -->
       </div> <!-- /header -->
@@ -30,30 +67,8 @@
       <div id="container" class="clearfix">
         <div id="main" class="column"><div id="main-squeeze">
             <div id="content">
-<?php
-
-
-$h1 = "Site under maintenance";
-$uri = ($_SERVER["HTTPS"] ? "https://" : "http://") .$_SERVER["HTTP_HOST"] .  $_SERVER["REQUEST_URI"];
-$msg = "The web site you are trying to reach is currently down for maintenance. Please try again later.";
-
-$filename = "/var/www/html/messages/" . escapeshellcmd( $_SERVER["REQUEST_URI"]);
-
-if (file_exists($filename)) {
-  $contents = file_get_contents($filename);
-  if ($contents != FALSE ) {
-    $lines = explode("\n", $contents);
-    $h1 = $lines[0];
-    $msg = '';
-    for($i=1;$i<count($lines); $i++) {
-      $msg .= "<p>" . $lines[$i] . "</p>";
-    }
-  }
-} 
-?>
-
               <h1 class="title" id="page-title"><?php echo $h1; ?></h1>
-              <h2><a href="<?php echo $uri;?>"><?php echo $uri;?></a>
+
               <div id="content-content" class="clearfix">
                 <?php echo $msg; ?> 
               </div> <!-- /content-content -->
